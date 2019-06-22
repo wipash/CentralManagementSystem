2054cccbd2506e65bb208a7146aeec71f87000e0
Anirudh Mungre <anirudh.mungre@uoit.net>
2019-06-22 18:32:35 -0400

    root hello world endpoint

---
+++ cms-back/app/app.ts
@@ -0,0 +1,12 @@
+import express = require('express');
+
+// Create a new express application instance
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Hello World app listening on port 3000!');
+});
