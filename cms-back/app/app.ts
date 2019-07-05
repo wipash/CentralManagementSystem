a6f4ca275bda7fc69ae1e598394fa9f7ea8385be
Sean McGrath <sean@mcgrath.net.nz>
2019-07-04 20:11:36 -0400

    Add port environment variable

--- cms-back/app/app.ts
+++ cms-back/app/app.ts
@@ -3,10 +3,12 @@ import express = require('express');
 // Create a new express application instance
 const app: express.Application = express();
 
+let port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Hello World app listening on port 3000!');
+app.listen(port, function () {
+  console.log("Listening on " + port);
 });
