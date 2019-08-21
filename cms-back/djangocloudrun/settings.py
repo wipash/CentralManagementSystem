7ac819ef28af471d342b0f0747763ce8dfcd732c
Chris Dryden <chrisdryden@Chriss-MacBook-Pro.local>
2019-08-21 14:50:48 -0400

    Improved testing speed

--- cms-back/djangocloudrun/settings.py
+++ cms-back/djangocloudrun/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zm9djizba@sv$z(7(&2lr8&nkooh)**pn=2klj9yi6#^(zt=w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
